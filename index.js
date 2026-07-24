ef1b46c2acdbd4919159ec509f6e2bb797389224
Vishal Singh <vs0252464@gmail.com>
2026-07-24 09:29:56 +0530

    Added JavaScript files

---
+++ index.js
@@ -0,0 +1,30 @@
+console.log("Hello Using Js");
+//var a = 10;
+//console.log("a=" + a);
+
+// let a = 78;
+// if (a > 50) {
+// let a = 100;
+// console.log("a inside the block=" + a);
+// }
+// console.log("a outside the block=" + a);
+// as an expression
+// const sum=function(a,b){
+//     return a+b;
+// }
+// const data=sum(10,20);
+// console.log("sum=" + data);
+// as an arrow function
+// const msg = (mymsg) => {
+//     console.log("hii" + mymsg);
+// }
+// msg("FSDAIMLC2026");
+// const mysqrt = (val) => 
+//     return math.sqrt(val);
+
+// const result = mysqrt(25);
+// console.log("Result=" + result);
+//let a = 10;
+//let b = 20;
+
+//console.log("Sum =", a + b);
