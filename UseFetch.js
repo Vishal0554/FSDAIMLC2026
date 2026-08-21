8299277246ac96d9c167d64490bbeb884fc07f9b
Vishal Singh <vs0252464@gmail.com>
2026-08-21 08:57:08 +0530

    Updated project

--- UseFetch.js
+++ UseFetch.js
@@ -1,9 +1,49 @@
-const div=document.getElementById('container');
-const btn=document.getElementById('btn');
+const div = document.getElementById('container');
+const button = document.getElementById('btn');
 console.log(div);
-  async function display(){
-    //div.innerHTML="<h2>Hello using dom</h2>";
-    const serverData= await fetch("https://fakestoreapi.com/products ");
-    console.log(serverData);
+const h2 = document.createElement("h2");
+h2.innerText = "Data is loading...";
+console.log(h2);
+
+function cart(ele){
+    alert("Cart is calling" + ele);
 }
-btn.addEventListener('click', display);
+async function display(){
+    try{
+    div.appendChild(h2);
+        // div.innerHTML='<h2 style="color:blue;">Hello Using DOM</h2>';
+    const serverdata =await fetch('https://fakestoreapi.com/products');
+    const jsondata = await serverdata.json();
+    // console.log(jsondata[1].title)
+    // div.innerHTML=`<h2 style="color:white;background-color:grey;"> ${JSON.stringify(jsondata)}</h2>`;
+
+    let table = `<table border=4px>
+        <tr>
+        <th>image</th>
+        <th>itemId</th>
+        <th>itemTitle</th>
+        <th>itemPrice</th>
+        </tr>
+        ${
+            jsondata.map((ele)=>(
+            `<tr>
+            <td><img src = ${ele.image} heigth = 40 width = 40></td>
+            <td>${ele.id}</td>
+            <td>${ele.title}</td>
+            <td>${ele.price}</td>
+            <td><button onclick = cart(${ele.id})>Add to Cart</button></td>
+            </tr>`
+            ))
+    }
+    </table>`
+    div.innerHTML = table;
+
+}catch(e){
+    console.log("error is:" + e);
+}
+finally{
+div.removeChild(h2);
+}
+}
+
+button.addEventListener('click',display);
