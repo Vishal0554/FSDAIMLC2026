85c98581f6cf0040c09dbbb917b25a031eb7d5b7
Vishal Singh <vs0252464@gmail.com>
2026-09-24 10:11:22 +0530

    Updated React project

---
+++ reactvite/reactvite/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+import ICard from './component/ICard'
+import ICardGallery from './component/ICardGallery'
+
+function App() {
+  
+
+  return (
+    <div style={{border:'2px solid black', height:'500px'}}>
+    
+       <ICardGallery />
+     
+    </div>
+  )
+}
+
+export default App
